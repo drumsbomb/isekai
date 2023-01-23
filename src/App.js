08e0c4280e5c579f3fdae312f02d8af35ef36691
safiullahrafeeq77777 <safiullahrafeeq7777@gmail.com>
2023-01-23 16:34:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { lazy,Suspense } from 'react'
+import { lazy, Suspense } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const Comic = lazy(() => import('pages/Comic/Comic'))
@@ -12,19 +12,19 @@ const Services = lazy(() => import('pages/Services/Services'))
 function App() {
   return (
     <>
-    <Suspense fallback={()=><h1>loading....</h1>}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/isekai" element={<Isekai />} />
+      <Suspense fallback={() => <h1>loading....</h1>}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/*<Route path="/isekai" element={<Isekai />} />
           <Route path="/comics" element={<Comic />} />
           <Route path="/isekai/project1" element={<Project1 />} />
           <Route path="/tav" element={<TAV />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/upload" element={<ContactUs />} />
-        </Routes>
-      </Router>
-    </Suspense>
+          <Route path="/upload" element={<ContactUs />} /> */}
+          </Routes>
+        </Router>
+      </Suspense>
     </>
   )
 }
